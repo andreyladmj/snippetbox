a890303381ada7f9d3f2f74ae7d0d754a451a0c6
Srivoknovski <test@gmail.com>
2019-12-30 23:23:51 +0200

    init

---
+++ cmd/web/handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet ID %d", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "method Not Allowed", 405)
+		return
+	}
+
+	fmt.Fprintf(w, "Create a new snippet")
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module andreyladmj/snippetbox
+
+go 1.12
